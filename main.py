f1cd424bcbaf15c12dca131ac5c06817a9b4b73f
PalmovuCocos <cocos.barchuck@yandex.ru>
2022-02-07 21:12:06 +0300

    add log_and

--- main.py
+++ main.py
@@ -13,5 +13,14 @@ def log_or(operand1,operand2):
     y = act(sum)
     return y
 
+def log_and(operand1, operand2):
+    x = np.array([operand1, operand2])
+    w1 = np.array([1, 1])
+    sum = np.dot(w1, x) - 1.5
+    print(sum,'сумма на нейроне')
+    y = act(sum)
+    return y
+
 
 print(log_or(0,0))
+print(log_and(1,1))
